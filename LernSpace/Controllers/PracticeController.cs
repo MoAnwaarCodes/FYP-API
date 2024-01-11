e62177cf38b986aef882cc4618b3d6e6870a6042
MoAnwaarCodes <moanwaar9@gmail.com>
2024-01-12 00:00:57 +0500

    First Commit

---
+++ LernSpace/Controllers/PracticeController.cs
@@ -0,0 +1,47 @@
+﻿using LernSpace.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LernSpace.Controllers
+{
+    public class PracticeController : ApiController
+    {
+       SlowlernerDbEntities db = new SlowlernerDbEntities();
+        [HttpPost]
+        public HttpResponseMessage AddNewPractic(List<collection> collection,Practic practic)
+        {
+            db.Practic.Add(practic);
+            practicCollection p = new practicCollection();
+            p.pracId = practic.PracId;
+            foreach (collection item in collection)
+            {
+                p.collectId = item.Collect_id;
+                db.practicCollection.Add(p);
+            }
+            db.SaveChanges();
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+        [HttpGet]
+        public HttpResponseMessage myPractices(int did)
+        {
+            var data=db.Practic.Where(e=>e.createBy == did)
+                .Join(db.practicCollection,practic=>practic.PracId,pracCollection=>pracCollection.pracId, (practic, pracCollection) => new { practic, pracCollection })
+                .Join(db.collection,ppc=>ppc.pracCollection.collectId,collect=>collect.Collect_id,(ppc,collect)=> new
+                {
+                    Collectid=collect.Collect_id,
+                    uText=collect.uText,
+                    eText=collect.eText,
+                    type=collect.type, 
+                    picPath=collect.picPath,
+                    C_grpup=collect.C_group
+                });
+            return Request.CreateResponse(HttpStatusCode.OK,data);
+        }
+    }
+}
---
+++ LernSpace/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using LernSpace.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace LernSpace.Controllers
+{
+    public class UserController : ApiController
+    {
+        SlowlernerDbEntities db= new SlowlernerDbEntities();
+        [HttpGet]
+        public HttpResponseMessage TodayAppointmrnts()
+        {
+            var request = HttpContext.Current.Request;
+            var Did =int.Parse( request["uid"]);
+            var data=db.appointment.Where(e=>e.userId == Did && e.nextAppointDate==DateTime.Now).ToList();
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+        [HttpPost]
+        public  HttpResponseMessage AddAppointment(appointment appoint)
+        {
+            appoint.appointmentDate = DateTime.Parse("12/12/2024");
+            appoint.nextAppointDate = DateTime.Now;
+            db.appointment.Add(appoint);
+            Console.WriteLine(appoint);
+            
+            db.SaveChanges();
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+        [HttpGet]
+        public HttpResponseMessage showSpacificAppointment(int id)
+        {
+            var data= db.appointment.Where(e=>e.app_Id == id).ToList();
+            return Request.CreateResponse(statusCode: HttpStatusCode.OK, data);
+        }
+    }
+}
